7365ba10889d8134e6833a9d63061ca884dcc467
I-Am-ChuckJ <55801787+I-Am-ChuckJ@users.noreply.github.com>
2023-07-28 08:04:44 -0500

    Add files via upload

---
+++ loganalytics-deployment.ps1
@@ -0,0 +1,16 @@
+﻿# Select your subscription
+Set-AzContext -Subscription "<your sub here>"
+
+# Set the resource group name and location
+$resourceGroupName = "<your rsg name here>"
+$location = "East US"
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Set the workspace name and SKU
+$workspaceName = "publicMediumGenAIExample"
+$sku = "pergb2018"
+
+# Create the Log Analytics workspace
+New-AzOperationalInsightsWorkspace -Location $location -Name $workspaceName -Sku $sku -ResourceGroupName $resourceGroupName
